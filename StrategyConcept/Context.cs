da2185e422d9650824ebf21ba39c061339a98536
HanenJMS <Hanensgamelab@gmail.com>
2022-08-27 16:25:46 -0400

    no message

---
+++ StrategyConcept/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.StrategyConcept
+{
+    public class Context
+    {
+        IStrategy strategy;
+
+        public void DoBusinessLogic()
+        {
+            //strategy.RunAlgorithm(object data);
+        }
+    }
+}
